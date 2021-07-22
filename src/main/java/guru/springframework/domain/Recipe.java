d691445eca090a6d283149bd9b2778d61753b306
rkopace <greenball@wp.pl>
2021-07-22 13:30:58 -0400

    Notes and Recipe before

---
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -0,0 +1,18 @@
+package guru.springframework.domain;
+
+public class Recipe {
+
+	   private String description;
+	    private Integer prepTime;
+	    private Integer cookTime;
+	    private Integer servings;
+	    private String source;
+	    private String url;
+	    private String directions;
+	    //todo add
+	    //private Difficulty difficulty;
+	    private Byte[] image;
+	    private Notes notes;
+	    
+	    
+}
